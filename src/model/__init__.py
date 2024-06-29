e168d7220ece61b8531389955320d83a1e8dcafc
Rafaelszc <rafaelbjj84@gmail.com>
2024-06-29 08:15:12 -0300

    dir init

---
+++ src/model/__init__.py
@@ -0,0 +1,4 @@
+from model.database import DataBase
+
+__author__= "Rafaelszc"
+__version__= "0.1.0"
---
+++ src/utils/__init__.py
@@ -0,0 +1,4 @@
+from utils.load_json import load_json
+
+__author__= "Rafaelszc"
+__version__= "0.1.0"
