e71df66817766231ffffb84dbd2eb777bdf7df86
Rafaelszc <rafaelbjj84@gmail.com>
2024-07-02 19:57:55 -0300

    REFRAC use __init__ method

--- src/controler/stores/kabum.py
+++ src/controler/stores/kabum.py
@@ -1,15 +1,23 @@
 from utils import get_response
-import re
 from bs4 import BeautifulSoup as bs
 
 class Kabum:
-    response = get_response(f'https://www.kabum.com.br/produto/112948/mouse-gamer-logitech-g203-lightsync-rgb-efeito-de-ondas-de-cores-6-botoes-programaveis-e-ate-8-000-dpi-preto-910-005793')
+    def __init__(self, url: str) -> None:
+        self.response = get_response(url)
 
-    site = bs(response.text, 'html.parser')
+        self.site = bs(self.response.text, 'html.parser')
 
-    @classmethod
-    def get_price(cls):
-        price_partition = cls.site.find('h4', attrs={'class': 'sc-5492faee-2 ipHrwP finalPrice'}).text
+    def get_product_url(self) -> list:
+        product_url_partition = self.site.find_all('a', attrs={'class': 'sc-9d1f1537-10 kueyFw productLink'}, href=True)
+
+        url_list = []
+        for a in product_url_partition:
+            url_list.append(f"https://www.kabum.com.br{a.get('href')}")
+
+        return url_list
+
+    def get_price(self) -> float:
+        price_partition = self.site.find('h4', attrs={'class': 'sc-5492faee-2 ipHrwP finalPrice'}).text
 
         symbol, price = price_partition.split()
 
@@ -17,19 +25,9 @@ class Kabum:
 
         return price
     
-    @classmethod
-    def get_name(cls):
-        name_partition = cls.site.find('h1', attrs={'class': 'sc-58b2114e-6 brTtKt'})
+    def get_name(self) -> str:
+        name_partition = self.site.find('h1', attrs={'class': 'sc-58b2114e-6 brTtKt'})
 
         name = name_partition.text
 
         return name
-    
-    @classmethod
-    def is_promotion(cls):
-        promotion_partition = cls.site.find('span', attrs={'class': 'sc-5492faee-1 ibyzkU oldPrice'})
-
-        if promotion_partition != []:
-            return True
-        
-        return False
