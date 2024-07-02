f5464fbaff992dd645a3b7f46036393a3a741092
Rafaelszc <rafaelbjj84@gmail.com>
2024-07-02 19:57:34 -0300

    REFRAC to __init__  method and ADD get_id() and get_values() functions

--- src/model/database.py
+++ src/model/database.py
@@ -3,34 +3,33 @@ from os import path
 from utils import load_json
 
 class DataBase:
-    database_rules = load_json(path.join('resources', 'rules', 'database_rules.json'))
+    def __init__(self) -> None:
+        self.database_rules = load_json(path.join('resources', 'rules', 'database_rules.json'))
 
-    connection = sqlite3.connect(path.join('resources', 'databases', 'stores_database.db'))
-    cursor = connection.cursor()
-    connection.row_factory = sqlite3.Row
+        self.connection = sqlite3.connect(path.join('resources', 'databases', 'stores_database.db'))
+        self.cursor = self.connection.cursor()
+        self.connection.row_factory = sqlite3.Row
 
-    @classmethod
-    def create_database(cls):
-        for mapping_json in cls.database_rules:
+    def create_database(self):
+        for mapping_json in self.database_rules:
             name_table = mapping_json['name_table']
             columns = ", ".join([f"{column_name} {column_type}" for column_name, column_type in mapping_json["columns"].items()])
 
-            cls.cursor.execute(f"DROP TABLE IF EXISTS {mapping_json['name_table']}")
+            self.cursor.execute(f"DROP TABLE IF EXISTS {mapping_json['name_table']}")
 
-            cls.cursor.execute(f"CREATE TABLE {name_table}({columns})")
+            self.cursor.execute(f"CREATE TABLE {name_table}({columns})")
 
-        cls.cursor.close()
-        cls.connection.close()
+        self.cursor.close()
+        self.connection.close()
     
-    @classmethod
-    def insert_values(cls, data: tuple, table: str):
+    def insert_values(self, data: tuple, table: str):
         try:
-            list_columns = list(next((item["columns"].keys() for item in cls.database_rules if item["name_table"] == table), None))
+            list_columns = list(next((item["columns"].keys() for item in self.database_rules if item["name_table"] == table), None))
             list_columns.remove('ID')
 
             quantity_columns = len(list_columns)
 
-            cls.cursor.execute(f"INSERT INTO {table} ({','.join(list_columns)}) VALUES ({','.join(['?']*quantity_columns)})", data)
+            self.cursor.execute(f"INSERT INTO {table} ({','.join(list_columns)}) VALUES ({','.join(['?']*quantity_columns)})", data)
 
         except TypeError:
             print(f"{table} table don't exists.")
@@ -39,7 +38,38 @@ class DataBase:
             print('Unexpected amount of data, different from that supported on column')
 
         else:
-            cls.connection.commit()
+            self.connection.commit()
 
-        cls.cursor.close()
-        cls.connection.close()
+        self.cursor.close()
+        self.connection.close()
+
+    def get_id(self, item: str, table: str) -> int:
+        column = table
+
+        try:
+            query_search = self.cursor.execute(f"SELECT ID FROM {table} WHERE {column} = ?", (item,))
+
+            required_id = [id for id in query_search][0]
+
+        except IndexError:
+            print(f"{table} hasn't {item} item")
+
+        else:
+            self.cursor.close()
+            self.connection.close()
+
+            return required_id[0]
+    
+    def get_values(self, table: str, head: int = None) -> list:
+        if head is not None and head > 0:
+            query_search = self.cursor.execute(f'SELECT * FROM {table} WHERE (ID > 0 AND ID <= {head})')
+
+            values = [value for value in query_search]
+
+            return values
+        else:
+            query_search = self.cursor.execute(f'SELECT * FROM {table}')
+
+            values = [value for value in query_search]
+
+            return values
