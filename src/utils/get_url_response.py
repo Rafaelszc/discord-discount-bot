975e4d24300551a6679cc972d71dd661e724d7ce
Rafaelszc <rafaelbjj84@gmail.com>
2024-07-26 00:46:34 -0300

    REFRAC add try cases

--- src/utils/get_url_response.py
+++ src/utils/get_url_response.py
@@ -1,8 +1,19 @@
 import requests
+from bs4 import BeautifulSoup as bs
 
-def get_response(url: str) -> requests.get:
-    header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
+def get_response(url: str) -> bs:
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
 
-    response = requests.get(url=url, headers=header)
+    try:
+        response = requests.get(url=url, headers=header)
+    
+    except requests.exceptions.URLRequired:
+        print(f"Error to connect url= '{url}'\nInsert the correct url")
 
-    return response
+    except requests.exceptions.ConnectTimeout:
+        return get_response(url)
+
+    else:
+        site = bs(response.content, 'html.parser')
+
+        return site
