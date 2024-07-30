9a3d0f4a6e0d42e42038f4b105b1eba3e0626391
Rafaelszc <rafaelbjj84@gmail.com>
2024-07-30 01:05:12 -0300

    REMOVE useless files

--- src/controler/stores/terabyte.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup as bs
-from utils import get_response
-
-def main():
-    response = get_response('https://www.terabyteshop.com.br/busca?str=mouse')
-
-    print(response)
--- src/model/__init__.py
+++
@@ -1,4 +0,0 @@
-from model.database import DataBase
-
-__author__= "Rafaelszc"
-__version__= "0.1.0"
