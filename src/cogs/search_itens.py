1696bdb6a3882785ed355547fc643f1f22b7806e
Rafaelszc <rafaelbjj84@gmail.com>
2024-10-21 23:04:29 -0300

    fix: move task start logic to init func

--- src/cogs/search_itens.py
+++ src/cogs/search_itens.py
@@ -5,23 +5,26 @@ from controler.stores.kabum import Kabum
 class SearchItens(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.search_itens.start()
         super().__init__()
+
+    def cog_unload(self):
+        self.search_itens.cancel()
 
     @tasks.loop(hours=5)
     async def search_itens(self):
-        await DataBase().remove_value('REQUESTS', None, 'all')
-        type_products = await DataBase().get_values('TYPES')
+        DataBase().remove_value('REQUESTS', None, 'all')
+        type_products = DataBase().get_values('TYPES')
 
         for request_product in type_products:
             kabum_response = Kabum(f'https://www.kabum.com.br/busca/{request_product[1]}')
 
-            url_list = await kabum_response.get_products_url()
+            url_list = kabum_response.get_products_url()
 
             for url in url_list:
-                await DataBase().insert_values((url,), "REQUESTS")
+                DataBase().insert_values((url,), "REQUESTS")
 
 async def setup(bot: commands.Bot):
     cog = SearchItens(bot)
-
-    await cog.search_itens.start()
+    
     await bot.add_cog(cog)
